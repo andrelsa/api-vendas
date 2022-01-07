d7190c99359e2f11d9ad5417bf659e310fd23b7e
André <andre.alves@oobj.com.br>
2022-01-07 16:51:21 -0300

    concluindo cadastro de clientes

--- src/main/java/github/com/andrelsa/SalesApplication.java
+++ src/main/java/github/com/andrelsa/SalesApplication.java
@@ -15,11 +15,36 @@ public class SalesApplication {
 	@Bean
 	public CommandLineRunner init(@Autowired ClientRepository clientRepository) {
 		return args -> {
+			System.out.println("Salvando clientes");
 			clientRepository.save(new Cliente("André"));
 			clientRepository.save(new Cliente("Gabi"));
 			
 			List<Cliente> clienteList = clientRepository.getAll();
 			clienteList.forEach(System.out::println);
+			
+			System.out.println("Atualizando clientes");
+			clienteList.forEach(cliente -> {
+				cliente.setName(cliente.getName() + " atualizado");
+				clientRepository.update(cliente);
+			});
+			
+			clienteList = clientRepository.getAll();
+			clienteList.forEach(System.out::println);
+			
+			System.out.println("Buscando clientes");
+			clientRepository.getByName("Gabi").forEach(System.out::println);
+			
+			/*System.out.println("Deletando clientes");
+			clientRepository.getAll().forEach(cliente -> {
+				clientRepository.delete(cliente);
+			});*/
+			
+			clienteList = clientRepository.getAll();
+			if (clienteList.isEmpty()) {
+				System.out.println("Nenhum cliente encontrado!!!!");
+			} else {
+				clienteList.forEach(System.out::println);
+			}
 		};
 	}
 	
--- src/main/java/github/com/andrelsa/domain/repository/ClientRepository.java
+++ src/main/java/github/com/andrelsa/domain/repository/ClientRepository.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import javax.persistence.criteria.CriteriaBuilder;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -13,6 +14,8 @@ import java.util.List;
 public class ClientRepository {
 	
 	private static String INSERT = "INSERT INTO CLIENTE (nome) VALUES (?)";
+	private static String UPDATE = "UPDATE CLIENTE SET NOME = ? WHERE ID = ?";
+	private static String DELETE = "DELETE FROM CLIENTE WHERE ID = ?";
 	private static String LIST_ALL = "SELECT * FROM CLIENTE";
 	
 	
@@ -24,15 +27,35 @@ public class ClientRepository {
 		return cliente;
 	}
 	
+	public Cliente update(Cliente cliente) {
+		jdbcTemplate.update(UPDATE, new Object[] {cliente.getName(), cliente.getId()});
+		return cliente;
+	}
+	
+	public void delete(Cliente cliente) {
+		delete(cliente.getId());
+		
+	}
+	
+	public void delete(Integer id) {
+		jdbcTemplate.update(DELETE, new Object[] {id});
+	}
+	
+	public List<Cliente> getByName(String name) {
+		return jdbcTemplate.query(LIST_ALL.concat(" WHERE NOME LIKE ? "), new Object[]{"%" + name + "%"},
+				getClientRowMapper());
+	}
+	
 	public List<Cliente> getAll() {
-		return jdbcTemplate.query(LIST_ALL, new RowMapper<Cliente>() {
-			@Override
-			public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
-				Integer id = resultSet.getInt("id");
-				String nome = resultSet.getString("nome");
-				return new Cliente(id, nome);
-			}
-		});
+		return jdbcTemplate.query(LIST_ALL, getClientRowMapper());
+	}
+	
+	private RowMapper<Cliente> getClientRowMapper() {
+		return (resultSet, i) -> {
+			Integer id = resultSet.getInt("id");
+			String nome = resultSet.getString("nome");
+			return new Cliente(id, nome);
+		};
 	}
 	
 }

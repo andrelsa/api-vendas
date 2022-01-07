0daa3b8775d4bdb25e3c5a85e9be6ec70bda3917
André <andre.alves@oobj.com.br>
2022-01-07 11:06:38 -0300

    injetando dependencias

---
+++ src/main/java/github/com/andrelsa/repository/ClientRepository.java
@@ -0,0 +1,13 @@
+package github.com.andrelsa.repository;
+
+import github.com.andrelsa.model.Client;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientRepository {
+	
+	public void persist(Client client) {
+		//acessa a base e salva o cliente
+	}
+	
+}

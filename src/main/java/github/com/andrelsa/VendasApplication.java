3948302107ad9248b715a0f3dfa0ecb274b75623
André <andre.alves@oobj.com.br>
2022-01-07 09:55:50 -0300

    configuração inicial do projeto

--- src/main/java/github/com/andrelsa/VendasApplication.java
+++ src/main/java/github/com/andrelsa/VendasApplication.java
@@ -1,2 +1,21 @@
-package github.com.andrelsa;public class VendasApplication {
+package github.com.andrelsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+	
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "hello world";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(VendasApplication.class, args);
+	}
+
 }

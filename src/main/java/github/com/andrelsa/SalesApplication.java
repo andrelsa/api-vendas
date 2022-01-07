8d6bf4a2447d528d55a3615c7ccb764ac163345d
André <andre.alves@oobj.com.br>
2022-01-07 14:52:58 -0300

    configurando e obtendo conexões com base de dados

--- src/main/java/github/com/andrelsa/SalesApplication.java
+++ src/main/java/github/com/andrelsa/SalesApplication.java
@@ -1,35 +1,13 @@
 package github.com.andrelsa;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication()
-@RestController
-public class VendasApplication {
-	
-	@Value("${application.name}")
-	private String applicationName;
-	
-	@Cachorro
-	private Animal animal;
-	
-	@Bean(name = "executarAnimal")
-	public CommandLineRunner executar(){
-		return args -> this.animal.makeNoise();
-	}
-	
-	@GetMapping("/hello")
-	public String helloWorld() {
-		return "hello world";
-	}
+public class SalesApplication {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(VendasApplication.class, args);
+		SpringApplication.run(SalesApplication.class, args);
 	}
 	
 }

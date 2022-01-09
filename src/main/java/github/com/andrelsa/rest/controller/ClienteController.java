4397d628a264183fe3c450783fc4fd2140026cf5
André <andre.alves@oobj.com.br>
2022-01-08 21:03:03 -0300

    requisição GET com parâmetros e Reponse Entity

--- src/main/java/github/com/andrelsa/rest/controller/ClienteController.java
+++ src/main/java/github/com/andrelsa/rest/controller/ClienteController.java
@@ -1,19 +1,32 @@
 package github.com.andrelsa.rest.controller;
 
+import github.com.andrelsa.domain.entity.Cliente;
+import github.com.andrelsa.domain.repository.ClienteRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/api/clientes")
 public class ClienteController {
 	
-	@RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+	private ClienteRepository clienteRepository;
+	
+	public ClienteController(ClienteRepository clienteRepository) {
+		this.clienteRepository = clienteRepository;
+	}
+	
+	@GetMapping("/api/clientes/{id}")
 	@ResponseBody
-	public String helloCliente(@PathVariable("nome") String nomeCliente) {
-		return String.format("Hello %s", nomeCliente);
+	public ResponseEntity getClienteById(@PathVariable Integer id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 	
 }

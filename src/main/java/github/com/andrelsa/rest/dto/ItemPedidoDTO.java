449afbd7454893b752b02bb5d17154b3332c6bd8
André <andre.alves@oobj.com.br>
2022-01-24 09:57:28 -0300

    Trabalhando com o modelo DTO

---
+++ src/main/java/github/com/andrelsa/rest/dto/ItemPedidoDTO.java
@@ -0,0 +1,8 @@
+package github.com.andrelsa.rest.dto;
+
+public class ItemPedidoDTO {
+	
+	private Integer produto;
+	private Integer quantidade;
+	
+}
---
+++ src/main/java/github/com/andrelsa/rest/dto/PedidoDTO.java
@@ -0,0 +1,12 @@
+package github.com.andrelsa.rest.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class PedidoDTO {
+	
+	private Integer cliente;
+	private BigDecimal total;
+	private List<ItemPedidoDTO> items;
+	
+}

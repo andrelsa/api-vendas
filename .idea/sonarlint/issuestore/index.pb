a5eb1808df756799695b825c6d5d1334764868a9
André <andre.alves@oobj.com.br>
2022-02-17 10:52:30 -0300

    Configurando as Roles e Authorities

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+w
+Gsrc/main/java/github/com/andrelsa/rest/controller/PedidoController.java,0\8\08552fd77b50164caa68ef44725ac32adeff58c5
+i
+9src/main/java/github/com/andrelsa/rest/dto/PedidoDTO.java,5\1\51976be41066d91cee287d1df1e6040372b8b8ea
+l
+<src/main/java/github/com/andrelsa/domain/entity/Cliente.java,c\4\c407d15a52a4725b0a3cf52f16a7fdcef7755117
+l
+<src/main/java/github/com/andrelsa/domain/entity/Produto.java,e\4\e4672f9a492dde92df434013c9500d531f192da3
+n
+>src/main/java/github/com/andrelsa/validation/NotEmptyList.java,4\7\473b5639e47a189c2f10c13a94c86b8a57101f65
+Œ
+\src/main/java/github/com/andrelsa/validation/constraintvalidation/NotEmptyListValidator.java,3\0\30eab81ab12d380d98c4f0c3a66ce692a7908ce4
+w
+Gsrc/main/java/github/com/andrelsa/config/InternacionalizacaoConfig.java,c\4\c48d9af72bd54669eec0a5400b14ccd2364afbe6
--- src/main/java/github/com/andrelsa/config/SecurityConfig.java
+++ src/main/java/github/com/andrelsa/config/SecurityConfig.java
@@ -9,20 +9,32 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser("fulano")
-				.password(passwordEncoder().encode("123")).roles("USER");
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
-	}
-	
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder()).withUser("fulano")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/clientes/**")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/api/pedidos/**")
+                .hasAnyRole("USER", "ADMIN")
+                .antMatchers("/api/produtos/**")
+                .hasRole("ADMIN")
+                .and()
+                .formLogin();
+    }
+
 }
